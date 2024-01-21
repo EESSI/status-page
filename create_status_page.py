c172747117b631fdea4aad7098ae809fa6367594
Terje Kvernes <terje@kvernes.no>
2024-01-21 22:53:24 +0100

    Use configuration files, use released scraper.
      - We now support a config file!
      - Also, we use cvmfs-server-scraper==0.0.3 as released.

--- create_status_page.py
+++ create_status_page.py
@@ -1,17 +1,71 @@
 #!/usr/bin/env python3
 
-import datetime
-import time
+import configparser
+import logging
+import sys
+from datetime import datetime
+from typing import Any, Dict, List, Union
 
+from cvmfsscraper import scrape, set_log_level
 from jinja2 import Environment, FileSystemLoader
 
-from cvmfsscraper.main import scrape, scrape_server
 
-env= Environment(loader=FileSystemLoader("templates/"))
+def read_config(file_path: str) -> Dict[str, List[str]]:
+    """
+    Reads configuration from an ini file, sets a valid logging level,
+    and raises an exception if an invalid logging level is provided.
+
+    :param file_path: Path to the configuration file.
+    :returns: Dictionary with configuration parameters and logging level.
+    :raises ValueError: If an invalid logging level is provided.
+    """
+    config = configparser.ConfigParser(allow_no_value=True)
+    config.read(file_path)
+
+    CONFIG_SECTIONS_THAT_MUST_HAVE_VALUES = ["stratum0", "stratum1"]
+
+    def get_section_keys(section: str) -> List[str]:
+        if section not in config:
+            raise ValueError(f"Section {section} not found in configuration file")
+
+        return [key for key in config[section]]
+
+    # Retrieve and validate logging level
+    logging_level_str = config.get("config", "logging_level", fallback="INFO").upper()
+    if logging_level_str not in logging._nameToLevel:
+        raise ValueError(f"Invalid logging level: {logging_level_str}")
+
+    logging_level = logging.getLevelName(logging_level_str)
+
+    conf: Dict[str, Union[List[str], int]] = {
+        "stratum0_servers": get_section_keys("stratum0"),
+        "stratum1_servers": get_section_keys("stratum1"),
+        "repos": get_section_keys("repositories"),
+        "ignore_repos": get_section_keys("ignored_repositories"),
+        "logging_level": logging_level,
+    }
+
+    for section in CONFIG_SECTIONS_THAT_MUST_HAVE_VALUES:
+        if not config[section]:
+            raise ValueError(f"Section {section} must have at least one value")
+
+    return conf
+
+
+config_path = "config.ini"
+try:
+    config = read_config(config_path)
+except ValueError as e:
+    print(f"Error in configuration: {e}")
+    sys.exit(1)
+
+set_log_level(config["logging_level"])
+
+env = Environment(loader=FileSystemLoader("templates/"))
 template = env.get_template("status.html.j2")
 
-NOW = time.time()
-ACCEPTED_TIME_SINCE_SNAPSHOT_IN_SECONDS = 60 * 60 * 3 # Three hours.
+NOW = datetime.now()
+ACCEPTED_TIME_SINCE_SNAPSHOT_IN_SECONDS = 60 * 60 * 3  # Three hours.
 
 LEGEND = {
     "OK": {
@@ -41,47 +95,44 @@ LEGEND = {
     },
 }
 
-def get_class(status):
+
+def get_class(status: str):
     return LEGEND[status]["class"]
 
-def get_text(status):
+
+def get_text(status: str):
     return LEGEND[status]["text"]
 
-def get_desc(status):
+
+def get_desc(status: str):
     return LEGEND[status]["description"]
+
 
 def ok_class():
     return get_class("OK")
 
+
 def degraded_class():
     return get_class("DEGRADED")
+
 
 def warning_class():
     return get_class("WARNING")
 
+
 def failed_class():
     return get_class("FAILED")
+
 
 def maintenance_class():
     return get_class("MAINTENANCE")
 
 
 servers = scrape(
-    stratum0 = [
-        "rug-nl.stratum0.cvmfs.eessi-infra.org",
-    ],    
-    stratum1 = [
-        "aws-eu-west1.stratum1.cvmfs.eessi-infra.org",
-        "azure-us-east1.stratum1.cvmfs.eessi-infra.org",
-        "bgo-no.stratum1.cvmfs.eessi-infra.org",
-        "rug-nl.stratum1.cvmfs.eessi-infra.org",
-        "broken.stratum1.cvmfs.eessi-infra.org",
-    ],
-    ignore_repos = [
-        "bla.eessi-hpc.org",
-        "bob.eessi-hpc.org",
-        "ci.eessi-hpc.org",
-    ],
+    stratum0_servers=config["stratum0_servers"],
+    stratum1_servers=config["stratum1_servers"],
+    repos=config["repos"],
+    ignore_repos=config["ignore_repos"],
 )
 
 # Contains events that are bad. 1 is degraded, 2 warning, 3 failure
@@ -107,32 +158,32 @@ stratum0_details = []
 
 stratum1_status_class = ok_class()
 stratum1_servers = [
-#    {
-#        "name": "bgo-no",
-#        "update_class": get_class("OK"),
-#        "geoapi_class": get_class("DEGRADED"),
-#    },
-#
-#    {
-#        "name": "rug-nl",
-#        "update_class": get_class("DEGRADED"),
-#        "geoapi_class": get_class("FAILED"),
-#    },
+    #    {
+    #        "name": "bgo-no",
+    #        "update_class": get_class("OK"),
+    #        "geoapi_class": get_class("DEGRADED"),
+    #    },
+    #
+    #    {
+    #        "name": "rug-nl",
+    #        "update_class": get_class("DEGRADED"),
+    #        "geoapi_class": get_class("FAILED"),
+    #    },
 ]
 
 repositories_status_class = ok_class()
 repositories = [
-#    {
-#        "name": "pilot",
-#        "revision_class": get_class("OK"),
-#        "snapshot_class": get_class("OK"),
-#    },
-#
-#    {
-#        "name": "ci",
-#        "revision_class": get_class("FAILED"),
-#        "snapshot_class": get_class("FAILED"),
-#    },
+    #    {
+    #        "name": "pilot",
+    #        "revision_class": get_class("OK"),
+    #        "snapshot_class": get_class("OK"),
+    #    },
+    #
+    #    {
+    #        "name": "ci",
+    #        "revision_class": get_class("FAILED"),
+    #        "snapshot_class": get_class("FAILED"),
+    #    },
 ]
 
 
@@ -174,12 +225,21 @@ for server in servers:
                 if rs == ok_class() or rs == degraded_class() or rs == failed_class():
                     repo_rev_status[repo.name] = warning_class()
 
-            if NOW - repo.last_snapshot > ACCEPTED_TIME_SINCE_SNAPSHOT_IN_SECONDS:
-                rs = repo_snap_status[repo.last_snapshot]
-                if rs == ok_class():
-                    repo_snap_status[repo.name] = degraded_class()
+            if repo.last_snapshot:
+                # Calculate the time difference as a timedelta and get the total seconds
+                time_difference = NOW - repo.last_snapshot
+                if (
+                    time_difference.total_seconds()
+                    > ACCEPTED_TIME_SINCE_SNAPSHOT_IN_SECONDS
+                ):
+                    rs = repo_snap_status[repo.last_snapshot]
+                    if rs == ok_class():
+                        repo_snap_status[repo.name] = degraded_class()
+            else:
+                repo_snap_status[repo.name] = failed_class()
+                repositories_not_ok_events.append(2)
 
-        shortname = server.name.split('.')
+        shortname = server.name.split(".")
 
         geoapi_class = default_class
         # 0 ok, 1 wrong answer, 2 failing, 9 unable to test
@@ -201,7 +261,7 @@ for server in servers:
         )
 
 for repo in known_repos:
-    shortname = repo.split('.')
+    shortname = repo.split(".")
     repositories.append(
         {
             "name": shortname[0],
@@ -211,7 +271,7 @@ for repo in known_repos:
     )
 
 for repo in stratum0_repo_versions:
-    shortname = repo.split('.')
+    shortname = repo.split(".")
     stratum0_details.append(shortname[0] + " : " + str(stratum0_repo_versions[repo]))
 
 
@@ -225,27 +285,21 @@ elif stratum1_not_ok_events:
     stratum1_status_class = degraded_class()
 
 
-
-data = {
+data: Dict[str, Any] = {
     "legend": LEGEND,
     "eessi_status_class": eessi_status_class,
     "eessi_status_text": eessi_status_text,
     "eessi_status_description": eessi_status_description,
-
     "stratum0_status_class": stratum0_status_class,
     "stratum0_details": stratum0_details,
-
     "stratum1_status_class": stratum1_status_class,
     "stratum1s": stratum1_servers,
-
     "repositories_status_class": repositories_status_class,
     "repositories": repositories,
-
-    "last_update": datetime.datetime.now().replace(microsecond=0).isoformat(),
+    "last_update": datetime.now().replace(microsecond=0).isoformat(),
 }
 
 output = template.render(data)
 
 with open("status-generated.html", "w") as fh:
     fh.write(output)
-
